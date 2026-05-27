a1c82479fe9da5ef34c2375f315f2d563f23a9aa
konga22 <qwopop2@gmail.com>
2026-05-27 23:10:44 +0900

    대기 화면 위젯 구조 분리

---
+++ lib/ui/pages/waiting/widgets/waiting_status_metrics.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
+
+import '../../../../app/extensions/spacing_extension.dart';
+import '../../../../models/mock_models.dart';
+import '../../../common/app_components.dart';
+
+class WaitingStatusMetrics extends StatelessWidget {
+  const WaitingStatusMetrics({super.key, required this.waiting});
+
+  final WaitingStatus waiting;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MetricTile(
+          label: '현재 대기',
+          value: '${waiting.currentTeams}',
+          icon: LucideIcons.users,
+        ),
+        12.widthBox,
+        MetricTile(
+          label: '예상 시간',
+          value: '${waiting.estimatedMinutes}',
+          icon: LucideIcons.clock,
+        ),
+      ],
+    );
+  }
+}
